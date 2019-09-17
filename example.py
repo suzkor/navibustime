1888c8f459e52cb346f97a246404fc9c04bd3745
suzkor <koheisuzukidf@gmail.com>
2019-09-17 18:45:25 +0900

    initial

--- example.py
+++ example.py
@@ -1,5 +1,12 @@
 import datetime
 from navibustime.navibustime import Navibustime
 
+# サンプル1
 bus = Navibustime("00016409", "00016648", "00004254")
 print(bus.nextbus(datetime.datetime.now()))
+# return
+
+# サンプル2
+bus = Navibustime("00016409", "00016648", "00004254")
+print(bus.nextbus(datetime.datetime.now()))
+# return
